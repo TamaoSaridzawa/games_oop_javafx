5c641efb79e7125cd6be059081b2f1433365e293
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-08-24 22:55:42 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -18,12 +18,35 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not move by diagonal from %s to %s", position, dest)
+            );
+        }
+        int size = position.getX() > dest.getX() ? position.getX() - dest.getX() :
+                dest.getX() - position.getX();
+        Cell[] steps = new Cell[size];
+        int x = position.getX();
+        int y = position.getY();
+        for (int i = 0; i < size; i++) {
+            x = x - dest.getX() < 0 ? x + 1 : x - 1;
+            y = y - dest.getY() < 0 ? y + 1: y - 1;
+            steps[i] = Cell.findBy(x, y);
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
+        if (source.getX() > dest.getX() && source.getY() < dest.getY()
+        || source.getX() < dest.getX() && source.getY() > dest.getY()) {
+            if (source.getX() - dest.getX() == dest.getY() - source.getY()) {
+                return true;
+            }
+        } else {
+            if (source.getX() - dest.getX() == source.getY() - dest.getY()) {
+                return true;
+            }
+        }
         return false;
     }
 
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.*;
+
+public class BishopBlackTest {
+
+    @Test
+    public void position() {
+        BishopBlack bishopBlack = new BishopBlack((Cell.F5));
+        Assert.assertThat(bishopBlack.position(), is(Cell.F5));
+    }
+
+    @Test
+    public void way() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.way(Cell.G5);
+        Cell[] array = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Assert.assertThat(bishopBlack.way(Cell.G5), is(array));
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void wayException() throws ImpossibleMoveException{
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.way(Cell.G3);
+        Cell[] array = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Assert.assertThat(bishopBlack.way(Cell.G5), is(array));
+    }
+
+    @Test
+    public void isDiagonal() {
+    }
+
+    @Test
+    public void copy() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.A4);
+        Assert.assertThat(bishopBlack.copy(Cell.A4).position(), is(bishopBlack.position()));
+    }
+}
